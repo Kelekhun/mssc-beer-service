40e7a13ba66e4cd921d320457ed71b23bfdb9550
Kelekhun <khadkadeepak@gmail.com>
2019-08-08 00:41:23 -0400

    Fixing Tests. Closes #11

--- src/test/java/com/kapeed/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/kapeed/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package com.kapeed.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kapeed.msscbeerservice.web.model.BeerDTO;
+import com.kapeed.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.sql.SQLOutput;
 import java.util.UUID;
 
@@ -34,7 +36,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -46,13 +48,22 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception {
 
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
 
          mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                  .contentType(MediaType.APPLICATION_JSON)
                  .content(beerDTOJson))
                  .andExpect(status().isNoContent());
+    }
 
+    BeerDTO getValidBeerDTO(){
+        return BeerDTO
+                .builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(123123123123L)
+                .build();
     }
 }
